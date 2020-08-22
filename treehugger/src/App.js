e6ae073167ec1121933cb59af5a0ca4c980cc43c
davisstacked <brettzvi@gmail.com>
2020-08-22 14:32:37 -0700

    updated APP.js

--- treehugger/src/App.js
+++ treehugger/src/App.js
@@ -1,12 +1,63 @@
 import React from 'react';
+import { withRouter } from 'react-router-dom';
+import jwt_decode from 'jwt-decode';
+import Routes from './config/routes';
+import NavBar from './components/NavBar/NavBar';
+import setAuthHeader from './utils/setAuthHeader';
 import './App.css';
 
-function App() {
-  return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    currentUser: localStorage.getItem('token'),
+  };
+
+  componentDidMount() {
+    const token = localStorage.getItem('token');
+    if (token) {
+      // Set Auth Header
+      setAuthHeader(token);
+      // Decode Token
+      const decodedToken = jwt_decode(token); 
+      // Set State
+      this.setState({currentUser: decodedToken.id})   
+    }
+  }
+
+  setCurrentUser = (token) => {
+    // Store Token
+    localStorage.setItem('token', token);
+    // Set Auth header
+    setAuthHeader(token);
+    // Decode Token
+    const decodedToken = jwt_decode(token);
+    // Set State
+    this.setState({currentUser: decodedToken.id})
+  };
+
+  logout = () => {
+    // Remove Token
+    localStorage.removeItem('token');
+    // Remove Auth Header
+    setAuthHeader();
+    // Set State
+    this.setState({currentUser: ''});
+    // Redirect
+    this.props.history.push('/login');
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <NavBar currentUser={this.state.currentUser} logout={this.logout} />
+        <div className="container">
+          <Routes currentUser={this.state.currentUser} setCurrentUser={this.setCurrentUser} />
+        </div>
+      </React.Fragment>
+    );
+  }
+
 }
 
-export default App;
+
+export default withRouter(App);
