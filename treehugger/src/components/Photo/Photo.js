8c657e93866116d25260fa78db5d471b962d7872
davisstacked <brettzvi@gmail.com>
2020-08-23 19:10:59 -0700

    finished Photo.js

---
+++ treehugger/src/components/Photo/Photo.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import './Photo.css';
+
+function Photo (props) {
+    const { photo, list } = props;
+
+    return (
+        <Link to={`/photos/${photo._id}`}>
+            <div className="photo-card">
+                <section className="title-wrapper">
+                    <h4>{photo.title}</h4>
+                </section>
+                <section className="image-wrapper">
+                    <img src={photo.image} alt={photo.title} height="100" />
+                </section>
+                <section className="content-wrapper">
+                    <p>{photo.caption}</p>
+                </section>
+            </div>
+        </Link>
+    );
+}
+
+export default Photo;
