c4bd0ef2cbb3de6b3ddbd97f0c49fa8d2a5cc6e3
davisstacked <brettzvi@gmail.com>
2020-08-23 19:39:50 -0700

    Completed Photo container.js

--- treehugger/src/pages/PhotoContainer/PhotoContainer.js
+++ treehugger/src/pages/PhotoContainer/PhotoContainer.js
@@ -1,3 +1,26 @@
 import React from 'react';
 import Photo from '../../components/Photo/Photo';
 import PhotoModel from '../../models/photo';
+
+
+class PhotoContainer extends React.Component {
+    state = {
+        photo: {},
+    };
+
+    componentDidMount() {
+        // Get All Photos
+        PhotoModel.getPhotoById(this.props.match.params.id)
+            .then((result) => {
+                console.log(result);
+            })
+            .catch((err) => console.log(err)) 
+    }
+
+    render() {
+        console.log(this.props);
+        return <Photo photo={this.state.photo} list={false} />;
+    }
+}
+
+export default PhotoContainer;
