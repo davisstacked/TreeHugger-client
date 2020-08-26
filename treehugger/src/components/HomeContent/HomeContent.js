347f1b48b58d563ab65aaeed03272179139e396e
davisstacked <brettzvi@gmail.com>
2020-08-26 10:46:59 -0700

    Got some more of the home page done

---
+++ treehugger/src/components/HomeContent/HomeContent.js
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import './HomeContent.css'
+
+function HomeContent () {
+    return (
+		<div class="about">
+			<h3>Become a TreeHugger</h3>
+			<div class="aboutHolder">
+				<div class="info">
+					<h4><b>Share your nature photos</b></h4>
+					<p>
+						Enjoy breathtaking vistas from around the globe. Share your nature photos with a community of nature lovers.
+					</p>
+				</div>
+
+				<div class="info">
+					<h4><b>Find events in your area</b></h4>
+					<p>
+						We have events in every U.S. state. Meet up virtually or in person
+						with those who have the same interests as you.
+					</p>
+				</div>
+
+				<div class="info">
+					<h4><b>Explore</b></h4>
+					<p>
+						Explore other people’s antique collections and findings. We have
+						over 1 million people posting pictures of their priceless pieces
+						every day.
+					</p>
+				</div>
+			</div>
+		</div>
+)}
+
+export default HomeContent;
--- treehugger/src/pages/Home/Home.js
+++ treehugger/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carosel from '../../components/Carosel/Carosel';
+import HomeContent from '../../components/HomeContent/HomeContent';
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
 
                     <h1>We're happy you're here.</h1>
                     <Carosel />
+                    <HomeContent />
         </div>
     );
 }
